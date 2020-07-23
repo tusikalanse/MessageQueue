980fe326e48aec40b4fdf11d5c8a72beb943103e
tusikalanse <615822545@qq.com>
2020-07-23 20:53:58 +0800

    MessageTable Header

---
+++ src/MessageTable.h
@@ -0,0 +1,15 @@
+#ifndef MESSAGE_TABLE_H
+#define MESSAGE_TABLE_H
+
+#include <memory>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/hash_policy.hpp>
+#include "NotACKMessage.h"
+
+class MessageTable {
+ public:
+ private:
+  
+}
+
+#endif
