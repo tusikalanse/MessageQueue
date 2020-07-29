dff84535bfe0645b982fbb14b988c33a05108fd2
tusikalanse <615822545@qq.com>
2020-07-29 15:15:33 +0800

    class broker

--- src/Broker.cpp
+++ src/Broker.cpp
@@ -1,23 +1,125 @@
+#include <cstring>
+#include <iostream>
+#include <string>
+#include <thread>
+#include <unistd.h>
 #include "Broker.h"
 
 Broker::Broker() : 
+    nextMessageID(0),
     refreshTimeout(1), 
     messageQueue(), 
     table(), 
+    port(8000),
     subscription()
   {}
 
-Broker::Broker(int priority) : 
+Broker::Broker(in_port_t _port) : 
+    nextMessageID(0),
+    refreshTimeout(1), 
+    messageQueue(), 
+    table(), 
+    port(_port),
+    subscription()
+  {}
+
+Broker::Broker(in_port_t _port, int priority) : 
+    nextMessageID(0),
     refreshTimeout(1), 
     messageQueue(priority), 
     table(), 
-    subscription() 
+    port(_port),
+    subscription()
   {}
 
-Broker::Broker(int priority, int _refreshTimeout) : 
+Broker::Broker(in_port_t _port, int priority, int _refreshTimeout) : 
+    nextMessageID(0),
     refreshTimeout(_refreshTimeout), 
     messageQueue(priority), 
     table(), 
+    port(_port), 
     subscription() 
   {}
 
+void Broker::run() {
+  int server_sockfd;
+  sockaddr_in server_addr;
+  memset(&server_addr, 0, sizeof(server_addr));
+  server_addr.sin_family = AF_INET;
+  server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+  server_addr.sin_port = htons(port);
+  
+  if ((server_sockfd = socket(PF_INET,SOCK_STREAM,0)) < 0) {  
+    perror("socket error");
+    return;
+  }
+
+  if (bind(server_sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
+    perror("bind error");
+    return;
+  }
+
+  if (listen(server_sockfd, 128) < 0) {
+    perror("listen error");
+    return;
+  }
+
+  std::thread listener(&Broker::server, this, server_sockfd);
+  listener.detach();
+
+  while (1) {
+    if (messageQueue.empty()) {
+      //sleep(1);
+      //std::cout << "empty message queue" << std::endl;
+      continue;
+    }
+    //std::cout << "message queue size is " << messageQueue.size() << std::endl;
+    std::cout << "nextMessageID is " << nextMessageID << std::endl;
+    std::shared_ptr<Message> message = messageQueue.top();
+    messageQueue.pop();
+    // TODO : ACK message
+    sendMessage(message);
+  }
+}
+
+void Broker::server(int server_sockfd) {
+  int client_sockfd;
+  sockaddr_in client_addr;
+  unsigned int sin_size = sizeof(client_addr);
+
+  while (1) {
+    //std::cout << "Waiting for connection" << std::endl;
+    int client_fd = accept(server_sockfd, (struct sockaddr *)&client_addr, &sin_size);
+    socketTable[nextUserID++] = client_fd;
+    std::thread client(&Broker::work, this, client_fd);
+    client.detach();
+  }
+}
+
+void Broker::work(int client_sockfd) {
+  send(client_sockfd, "Welcome to my MQ!\n", 18, 0);
+  int len;
+  char buf[BUFSIZ];
+  //std::cout << "Connection established" << std::endl;
+  while ((len = recv(client_sockfd, buf, BUFSIZ, 0)) > 0) {
+    buf[len] = '\0';
+    //std::cout << buf << std::endl;
+    messageQueue.push(getMessage(buf));
+    //std::cout << "KJFDJSLKJFDKLJ" << std::endl;
+  }
+  //std::cout << "QUIT" << std::endl;
+}
+
+std::shared_ptr<Message> Broker::getMessage(char str[]) {
+  return std::make_shared<Message>(Message(0, rand() % 10, str, nextMessageID++));
+}
+
+void Broker::sendMessage(std::shared_ptr<Message> message) {
+  for (std::pair<int, bool> UserID : subscription.getUsers(message->topic)) {
+    int clientID = socketTable[UserID.first];
+    if (send(clientID, message->message.c_str(), message->message.size(), 0) < 0) {
+      perror("write error");
+      return;
+    }
+  }
+}
--- src/Broker.h
+++ src/Broker.h
@@ -1,6 +1,13 @@
 #ifndef BROKER_H
 #define BROKER_H
 
+#include <cstdio>
+#include <arpa/inet.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/hash_policy.hpp>
 #include "MessageTable.h"
 #include "MyMessageQueue.h"
 #include "Subscription.h"
@@ -8,14 +15,21 @@
 class Broker {
  public:
   Broker();
-  Broker(int priority);
-  Broker(int priority, int _refreshTimeout);
-  void ACK(const int messageID, const int UserID);
-  void receiveMessage(const std::shared_ptr<Message>& message);
-  
+  Broker(in_port_t _port);
+  Broker(in_port_t _port, int priority);
+  Broker(in_port_t _port, int priority, int _refreshTimeout);
+  void run();
+  void server(int server_sockfd);
+  void work(int client_sockfd);
+  std::shared_ptr<Message> getMessage(char str[]);
+  void sendMessage(std::shared_ptr<Message> message);
  private:
+  int nextUserID;
+  int nextMessageID;
+  const in_port_t port;
   const int refreshTimeout;
   MyMessageQueue messageQueue;
+  __gnu_pbds::gp_hash_table<int, int> socketTable;
   MessageTable table; 
   Subscription subscription;
 };
