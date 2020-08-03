039f76730269f37a78485ea57204ce5c0cba6cd6
tusikalanse <615822545@qq.com>
2020-08-03 20:39:26 +0800

    add some network functions

--- src/network.cpp
+++ src/network.cpp
@@ -1,16 +1,40 @@
 #include "network.h"
 
+#include <arpa/inet.h>
 #include <cstdio>
+#include <errno.h>
 #include <fcntl.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <sys/types.h>
 #include <unistd.h>
 
 void network::setnonblocking(int sockfd) {
   int flag = fcntl(sockfd, F_GETFL, 0);
   if (flag < 0) {
-      perror("fcntl F_GETFL fail");
-      return;
+    perror("fcntl F_GETFL fail");
+    return;
   }
   if (fcntl(sockfd, F_SETFL, flag | O_NONBLOCK) < 0) {
-      perror("fcntl F_SETFL fail");
+    perror("fcntl F_SETFL fail");
   }
+}
+
+int network::read(int sockfd, char* buf) {
+  int res = 0;
+  while (1) {
+    int ret = recv(sockfd, buf, BUFSIZ, 0);
+    res += ret;
+    if (ret < 0) {
+      if ( (errno == EAGAIN) || (errno == EWOULDBLOCK)) {
+        break;
+      }
+      close(sockfd);
+      break;
+    }
+    else if (ret == 0) {
+      close(sockfd);
+    }  
+  }
+  return res;
 }
--- src/network.h
+++ src/network.h
@@ -5,6 +5,7 @@ namespace network {
   void setnonblocking(int sockfd);
   int read(int sockfd);
   int send(int sockfd, const char* buf, int len);
+  void setOutput(int sockfd, bool enableOutput);
 }
 
 #endif /* NETWORK_H */
